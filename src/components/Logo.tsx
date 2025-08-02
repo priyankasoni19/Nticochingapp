499db91b18d79a71f55e0e81d1b7678a0b4fcb94
priyanka soni <sonipriyanka056@gmail.com>
2025-08-02 12:04:03 +0000

    upload this logo for NTI COCHING APP ICON

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,18 +8,21 @@ export function Logo({ className }: { className?: string }) {
         viewBox="0 0 200 200"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        className="text-primary"
       >
         <path
-          d="M49.748 158.465V41.535H69.418V86.735L108.318 41.535H134.148L96.888 88.455L136.218 158.465H109.518L83.808 115.125L76.248 122.895V158.465H49.748Z"
-          fill="currentColor"
+          d="M25.8 135.9V43.1H49.5V85.9L92.1 43.1H120.4L78.1 88.4L122.8 135.9H94.1L66.7 101.9L58.5 110.1V135.9H25.8Z"
+          fill="hsl(var(--primary))"
         />
         <rect
-          x="144.988"
-          y="41.535"
-          width="19.67"
-          height="116.93"
-          fill="currentColor"
+          x="131.7"
+          y="43.1"
+          width="23.7"
+          height="92.8"
+          fill="hsl(var(--primary))"
+        />
+        <path
+          d="M152.9 40.1C102.5 40.1 85.7 70.3 75.8 91.3C104.2 84.6 132.6 77.9 161 71.2C165.7 85.9 160.8 103.1 149.3 113.8C127.3 134.1 90.9 125.7 75.8 110.6C95.9 127.5 125.9 143.6 161.8 143.6C206.8 143.6 226.5 109.8 226.5 86.9C226.5 58.6 193.3 40.1 152.9 40.1Z"
+          fill="#E51937"
         />
       </svg>
       <h1 className="text-2xl font-bold text-primary">NTI COCHING APP</h1>
